53abcccaf1de33c474373e955fa040b15e499ca5
poojashree <poojarajappa1234@gmail.com>
2020-11-19 13:52:14 +0530

    kiuyh

---
+++ src/main/java/com/poojashree/springboot/WebbbbApplication.java
@@ -0,0 +1,13 @@
+package com.poojashree.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbbbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poojashree/springboot/WebbbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poojashree.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
